5022ff75ab89b43dd7cec429a2ff857a7344b177
Zach Estrella <zachgabriel27@gmail.com>
2026-05-10 22:31:52 +0800

    Add button opacity on hover

--- components/reusable/Button.tsx
+++ components/reusable/Button.tsx
@@ -10,7 +10,7 @@ const Button = ({children,className,type}:ButtonProps) => {
     return (
         <div className={clsx("min-w-fit rounded-md flex items-center gap-2 cursor-pointer",
             className ?? `${className}`,
-            type == "primary" && "btn btn-info text-white",
+            type == "primary" && "btn btn-info text-white hover:opacity-80",
             type == "secondary" && "border border-gray-300 px-2 py-1 text-xs hover:border-info hover:text-info",
             type == "tertiary" && "border border-white bg-white text-black px-4 py-1 text-xs hover:border-info hover:text-info"
         )}>
