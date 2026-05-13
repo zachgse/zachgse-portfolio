658853be4ddbab7bea7ac2abfe5b7e9f50742180
Zach Estrella <zachgabriel27@gmail.com>
2026-05-13 20:54:11 +0800

    Wrap project single content params into promise for cached fetch

--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -5,19 +5,19 @@ import ProjectSingleContent from "@/components/features/Project/ProjectSingleCon
 import { cachedFetch } from "@/supabase/custom";
 
 export async function generateMetadata(
-  { params }: { params: { slug: string } }
+    { params }: { params: Promise<{ slug: string }> }
 ): Promise<Metadata> {
-    const slug = await params;
-    const format = slug.slug.charAt(0).toUpperCase() + slug.slug.slice(1);
+    const {slug} = await params;
+    const format = slug.charAt(0).toUpperCase() + slug.slice(1);
     return {
         title: `Zach Estrella | ${format}`,
         description: `Project description of ${format} project`,
     };
 }
 
-const ProjectSlug = async({params}:{params:{slug:string}}) => {
-    const slug = await params;
-    const projects = await cachedFetch(`projects?slug=eq.${await slug.slug}&limit=1`);
+const ProjectSlug = async({ params }: { params: Promise<{ slug: string }> }) => {
+    const {slug} = await params;
+    const projects = await cachedFetch(`projects?slug=eq.${slug}&limit=1`);
     const project = projects?.[0] ?? null;
 
     if (!slug || !project) return <Error/>
