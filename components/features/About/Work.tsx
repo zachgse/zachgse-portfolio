1465abe66d1234829c487dc4b42196aab213f2d5
Zach Estrella <zachgabriel27@gmail.com>
2026-05-13 20:36:30 +0800

    Finalize new resume and updated work experience

--- components/features/About/Work.tsx
+++ components/features/About/Work.tsx
@@ -8,13 +8,13 @@ const Work = () => {
                 <p className="font-bold text-2xl">Work Experience</p>
             </div>
             <div className="flex gap-4">
-                <img src="../assets/img/pmti.png" alt="Pilipinas Micro Matrix Inc. logo" className="w-16 h-16"/>
+                <img src="../assets/img/ziaplex.jpg" alt="Ziaplex logo" className="w-16 h-16"/>
                 <div className="flex flex-col gap-1">
                     <p className="font-semibold">Web Developer</p>
-                    <p className="text-xs font-semibold">Pilipinas Micro Matrix Inc.</p>
+                    <p className="text-xs font-semibold">Ziaplex Inc.</p>
                     <p className="text-gray-400 text-xs dark:text-white flex items-center gap-1">
                         <Calendar size={12}/>
-                        Aug 2022 - Nov 2025
+                        Aug 2022 - Oct 2025
                     </p>
                 </div>
             </div>
